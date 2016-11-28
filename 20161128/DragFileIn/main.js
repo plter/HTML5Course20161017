e23a1c42e99eee6002ef70322b27bb6d430ab9b2
梦宇 <xtiqin@163.com>
2016-11-28 14:30:08 +0800

    文件拖入

---
+++ 20161128/DragFileIn/main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by plter on 2016/11/28.
+ */
+
+(function () {
+
+    $("#area").on("dragover", function (e) {
+        e.preventDefault();
+    }).on("drop", function (e) {
+        e.preventDefault();
+        var self = this;
+
+        // console.log(e.originalEvent.dataTransfer.files);
+
+
+        var files = e.originalEvent.dataTransfer.files;
+        if (files && files.length) {
+            var first = files[0];
+
+            console.log(first);
+
+            var reader = new FileReader();
+            reader.onload = function () {
+                self.innerHTML = reader.result;
+            };
+            reader.readAsText(first);
+        }
+    });
+
+})();
