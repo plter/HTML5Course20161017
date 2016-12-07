91c1175c5084b97b9f419d412f64dc4fbf016130
梦宇 <xtiqin@163.com>
2016-12-07 10:13:48 +0800

    异步编程

---
+++ 20161207/Promise/main.js
@@ -0,0 +1,76 @@
+/**
+ * Created by plter on 2016/12/7.
+ */
+
+(function () {
+
+    // $.get("data.txt").done(function (data) {
+    //     console.log(data);
+    // }).fail(function (err) {
+    //     console.log(err);
+    // });
+
+    // $.get("data.txt").done(function (data) {
+    //     console.log(data);
+    //
+    //     $.get("data1.txt").done(function (data) {
+    //         console.log(data);
+    //
+    //         $.get("data2.txt").done(function (data) {
+    //             console.log(data);
+    //         }).fail(function (err) {
+    //             console.log(err);
+    //         })
+    //     }).fail(function (err) {
+    //         console.log(err);
+    //     });
+    // }).fail(function (err) {
+    //     console.log(err);
+    // });
+
+    function loadData(url) {
+        return new Promise(function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                resolve(xhr.responseText);
+            };
+            xhr.onerror = function (err) {
+                reject(err);
+            };
+            xhr.open("get", url);
+            xhr.send();
+        });
+    }
+
+    loadData("data.txt").then(function (result) {
+        console.log(result);
+
+        return loadData("data1.txt");
+    }).then(function (result) {
+        console.log(result);
+
+        return loadData("data2.txt");
+    }).then(function (result) {
+        console.log(result);
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+
+    // new Promise(function (resolve, reject) {
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onload = function () {
+    //         resolve(xhr.responseText);
+    //     };
+    //     xhr.onerror = function (err) {
+    //         reject(err);
+    //     };
+    //     xhr.open("get", "data.txt");
+    //     xhr.send();
+    // }).then(function (result) {
+    //     console.log(result);
+    // }).catch(function (error) {
+    //     console.log(error);
+    // });
+
+})();
