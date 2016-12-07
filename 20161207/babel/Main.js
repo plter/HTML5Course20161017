f4ade1e62343afb43e23a643c1cc69704850b16e
梦宇 <xtiqin@163.com>
2016-12-07 16:37:11 +0800

    使用babel编译es6成es5

---
+++ 20161207/babel/Main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by plter on 2016/12/7.
+ */
+
+class Main {
+    constructor() {
+        this.sayHello();
+    }
+
+    sayHello() {
+        console.log("Hello World");
+    }
+}
+
+new Main();
