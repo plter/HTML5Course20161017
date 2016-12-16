94317056832bac392ecbe6e552004f9741d56345
梦宇 <xtiqin@163.com>
2016-12-16 10:55:47 +0800

    模板

--- 20161216/HelloAngular/app.js
+++ 20161216/HelloAngular/app.js
@@ -4,4 +4,11 @@
 
 angular.module("app", []).controller("main", function ($scope) {
     $scope.word = "Hello";
+
+    $scope.students = [
+        {name: "张三", age: 1},
+        {name: "李四", age: 3},
+        {name: "王五", age: 5},
+        {name: "赵六", age: 2}
+    ];
 });
