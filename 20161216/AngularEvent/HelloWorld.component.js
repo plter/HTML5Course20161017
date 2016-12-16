42ae77f9b99f2cc5f601fa1be8b110cf524bcfa0
梦宇 <xtiqin@163.com>
2016-12-16 15:19:19 +0800

    ajax

--- 20161216/AngularEvent/HelloWorld.component.js
+++ 20161216/AngularEvent/HelloWorld.component.js
@@ -6,6 +6,6 @@ angular.module("app").component("helloWorld", {
     // template: "<h1>{{word}}</h1>",
     templateUrl: "HelloWorld.template.htm",
     controller: function ($scope) {
-        $scope.word = "Hello World 1";
+        $scope.word = "Hello World";
     }
 });
--- 20161216/HelloAngular/app.js
+++ 20161216/HelloAngular/app.js
@@ -2,15 +2,8 @@
  * Created by plter on 2016/12/16.
  */
 
-angular.module("app", []).controller("main", function ($scope, $interval) {
+angular.module("app", []).controller("main", function ($scope, $http, $interval) {
     $scope.word = "Hello";
-
-    $scope.students = [
-        {name: "张三", age: 1},
-        {name: "李四", age: 3},
-        {name: "王五", age: 5},
-        {name: "赵六", age: 2}
-    ];
 
     $scope.count = 0;
     $scope.text = "Hello World";
@@ -18,4 +11,8 @@ angular.module("app", []).controller("main", function ($scope, $interval) {
     $interval(function () {
         $scope.count++;
     }, 1000);
+
+    $http.get("data.json").then(function (result) {
+        $scope.students = result.data;
+    });
 });
